f6768f2c6b2f6a949a4e09ab8aa993f1cbb80d5d
Pumpkin <klemon108@163.com>
2020-12-16 17:22:10 +0800

    p3

--- all_in_one_line.py
+++ all_in_one_line.py
@@ -1,6 +1,7 @@
-f = open('mua/Main.java','r')
+f = open('Main.java','r')
 data = f.read()
 lines = data.split('\n')
+lines = [l for l in lines if not l.strip().startswith('//')]
 s = ''
 for line in lines:
     s += line.strip()
